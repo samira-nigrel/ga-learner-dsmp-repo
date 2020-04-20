412f3a55ab7413bb4f02ccce583de4cb15f7de2d
samira-nigrel <samira-nigrel@users.noreply.github.com>
2020-04-20 21:08:54 +0530

    Code file created.

---
+++ Data-Science/code.py
@@ -0,0 +1,65 @@
+# --------------
+# Importing header files
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+
+#Reading file
+data = np.genfromtxt(path, delimiter=",", skip_header=1)
+
+#Code starts here
+
+#step 1
+census=np.concatenate((data, new_record),axis = 0)
+print(census.shape)
+
+#step 2
+age=census[:,0]
+max_age=age.max()
+print("Max Age= ",max_age)
+min_age=age.min()
+print("Min Age= ",min_age)
+age_mean=age.mean()
+print("Age Average= ", age_mean)
+age_std=age.std()
+print("Age Standard Deviation= ",age_std)
+
+#step 3
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+
+race_list=[len_0, len_1,len_2, len_3, len_4]
+minority_race=race_list.index(min(race_list))
+print(minority_race)
+
+#step 4
+senior_citizens=census[census[:,0]>60]
+#print(senior_citizens)
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+senior_citizens_len=len(senior_citizens)
+avg_working_hours=working_hours_sum/senior_citizens_len
+print(working_hours_sum,avg_working_hours)
+
+#step 5
+high=census[census[:,1]>10]
+avg_pay_high=high[:,7].mean()
+print(avg_pay_high)
+low=census[census[:,1]<=10]
+avg_pay_low=low[:,7].mean()
+print(avg_pay_low)
+
+
+
